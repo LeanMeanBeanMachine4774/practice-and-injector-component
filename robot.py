7ee34400f1f8ee824e559457b6e22608494e1f64
Beana1234 <asher.broadhead@thedropbears.org.au>
2024-10-31 22:24:02 +1100

    create robotpy scaffold

---
+++ robot.py
@@ -0,0 +1,18 @@
+import magicbot
+import wpilib
+
+class MyRobot (magicbot.MagicRobot):
+    def createObjects(self):
+        pass
+    
+    def teleopInit(self):
+        pass
+    
+    def teleopPeriodic(self):
+        pass
+    
+    
+    
+    
+if __name__ == "__main__":
+    wpilib.run(MyRobot)
