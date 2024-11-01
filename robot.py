5f607bdc7cebadf49000a810cdc0d4a8f6e9199b
LeanMeanBeanMachine4774 <asher.broadhead@thedropbears.org.au>
2024-11-01 13:33:23 +1100

    create controller object

--- robot.py
+++ robot.py
@@ -1,18 +1,17 @@
 import magicbot
 import wpilib
 
-class MyRobot (magicbot.MagicRobot):
-    def createObjects(self):
+
+class MyRobot(magicbot.MagicRobot):
+    def createObjects(self) -> None:
+        self.controller = wpilib.XboxController(0)
+
+    def teleopInit(self) -> None:
         pass
-    
-    def teleopInit(self):
+
+    def teleopPeriodic(self) -> None:
         pass
-    
-    def teleopPeriodic(self):
-        pass
-    
-    
-    
-    
+
+
 if __name__ == "__main__":
     wpilib.run(MyRobot)
