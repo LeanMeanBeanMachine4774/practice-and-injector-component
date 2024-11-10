f2cfa540c8788ddc96d26b863b7fb1347d76ff59
LeanMeanBeanMachine4774 <asher.broadhead@thedropbears.org.au>
2024-11-10 19:04:13 +1100

    fill out injector component

---
+++ components/injector.py
@@ -0,0 +1,17 @@
+from rev import CANSparkMax
+from wpilib import DigitalInput
+
+class InjectorComponent:
+    def __init__(self) -> None:
+        self.injectormotor = CANSparkMax(1, CANSparkMax.MotorType.kBrushless)
+        self.desired_injector_speed = 0.0
+        CANSparkMax.setInverted(self.injectormotor, False)
+        self.breakbeam = DigitalInput(2)
+        
+    def intaking(self) -> None:
+        self.desired_injector_speed = 0.5
+        
+    def execute(self) -> None:
+        self.injectormotor.set(self.desired_injector_speed)
+        if (not self.breakbeam.get()):
+            self.desired_injector_speed = 0.0
