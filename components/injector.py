cc4c7c1aab635b4bec80aa535e0cc8ac2f707345
LeanMeanBeanMachine4774 <asher.broadhead@thedropbears.org.au>
2024-11-12 16:20:36 +1100

    set injector idle to brake

--- components/injector.py
+++ components/injector.py
@@ -5,8 +5,9 @@ from wpilib import DigitalInput
 class InjectorComponent:
     def __init__(self) -> None:
         self.injectormotor = CANSparkMax(1, CANSparkMax.MotorType.kBrushless)
-        self.desired_injector_speed = 0.0
         CANSparkMax.setInverted(self.injectormotor, False)
+        self.injectormotor.setIdleMode(CANSparkMax.IdleMode.kBrake)
+        self.desired_injector_speed = 0.0
         self.breakbeam = DigitalInput(2)
 
     def has_note(self) -> bool:
