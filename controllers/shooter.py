49e22b2520a223c78775f10913ba2ec1775b6d31
LeanMeanBeanMachine4774 <asher.broadhead@thedropbears.org.au>
2024-11-12 16:23:04 +1100

    write shooting state

--- controllers/shooter.py
+++ controllers/shooter.py
@@ -1,4 +1,4 @@
-from magicbot import StateMachine
+from magicbot import StateMachine, timed_state, state, default_state, will_reset_to
 from components.shooter import ShooterComponent
 from components.injector import InjectorComponent
 
@@ -6,3 +6,26 @@ from components.injector import InjectorComponent
 class Shooter(StateMachine):
     shooter: ShooterComponent
     injector: InjectorComponent
+    outtake_desire = will_reset_to(False)
+
+    def try_outtake(self) -> None:
+        self.outtake_desire = True
+
+    @default_state()
+    def flywheel_state(self) -> None:
+        if self.injector.has_note:
+            self.shooter.spin_up_flywheels()
+        else:
+            self.shooter.coast_flywheels()
+        if self.outtake_desire:
+            self.engage()
+
+    @state(first=True, must_finish=True)
+    def outtake(self) -> None:
+        self.injector.injecting()
+        if not self.injector.has_note:
+            self.next_state(self.let_note_travel)
+
+    @timed_state(0.5, must_finish=True, next_state="done")
+    def let_note_travel(self) -> None:
+        pass
